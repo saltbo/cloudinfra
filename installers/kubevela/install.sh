793a34a66c28694248b7acdba22a7d5e1e3c7d96
saltbo <saltbo@foxmail.com>
2022-02-11 19:06:27 +0800

    feat: add vela apps

--- installers/kubevela/install.sh
+++ installers/kubevela/install.sh
@@ -2,4 +2,4 @@
 
 helm repo add kubevela https://charts.kubevela.net/core
 helm repo update
-helm install --create-namespace -n vela-system --set admissionWebhooks.certManager.enabled=true kubevela kubevela/vela-core  --version 1.2.2 --wait
+helm install --create-namespace -n vela-system kubevela kubevela/vela-core  --version 1.2.2 --wait
